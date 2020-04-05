8581b56db88a09cf6c2d2487183aed6ab01bcd1d
MoogMt <moog.mathieu@gmail.com>
2020-04-05 23:50:45 +0200

    new file:   caracole.py 	new file:   player.py

---
+++ caracole.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 16:45:20 2020
+
+@author: moogmt
+"""
+
+import cards
+import player
+
+# Get Point Values
+#=============================================================================#
+def getPointValue( card ):
+    max_n_point = len( cards.standard_values )
+    for i_val in range( max_n_point ):
+        if cards.standard_values[i_val] == card.value:
+            if i_val == 8:
+                return 0
+            elif i_val == 0 :
+                return 1
+            else:
+                return max_n_point - i_val - 1
+def getPointValueHand( hand ):
+    points = 0
+    for card in hand:
+        points += getPointValue( card )
+    return points
+#=============================================================================#
+
+#=============================================================================#
+class CaracolePlayer( Player ):
+    """
+    Class defining a game server
+        -> username : username of the player
+        -> ip_adr : IP adress of the player
+    """
+    """
+        Builder
+    """
+    def __init__( self, username, ip, hand ) :
+        self.username = username
+        self.ip_adr = ip
+        self.hand = hand
+        return
+    """  
+        Accessors methods
+    """
+    def get_hand( self ):
+        """Method that returns the shape'"""
+        return self.hand
+    def get_pointValueHand( self ):
+        """ Method get """
+        return getPointValueHand( self.get_hand() )
+    def get_all( self ):
+        """Method that returns all informations"""
+        return self.username, self.ip_adr, self.hand
+    """
+        Modifyers
+    """
+    def set_hand( self, new_hand ):
+        """Method that modifies IP of the user"""
+        self.hand = new_hand
+        return
+    def set_all( self, new_username, new_ip, new_hand ):
+        self.set_username( new_username )
+        self.set_ip( new_ip )
+        self.set_hand( new_hand )
+        return
+    """
+        Printers
+    """
+    def print_hand( self ):
+        print("Cards: ")
+        for card in self.hand: 
+            card.print_all()
+        return
+    def print_all( self ):
+        print("username: " + str(self.get_username())+"; ip: " + str(self.get_ip()) +  ";" )
+        self.print_hand()
+        return    
+#=============================================================================#
+
+
+
+def caracoleGame( n_players, n_shuffle, n_deck, n_shuffle ):
+    
+    return 
---
+++ player.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 00:49:44 2020
+
+@author: moogmt
+"""
+
+class Player:
+    """
+    Class defining a game server
+        -> username : username of the player
+        -> ip_adr : IP adress of the player
+    """
+    
+    # Builder
+    def __init__( self, username, ip ) :
+        self.username = username
+        self.ip_adr = ip
+        return
+        
+    # Accessors methods
+    def get_username( self ):
+        """Method that returns the color"""
+        return self.username
+    def get_ip( self ):
+        """Method that returns the shape'"""
+        return self.ip_adr
+    def get_all( self ):
+        """Method that returns all informations"""
+        return self.username, self.ip_adr
+    
+    # Modifyers
+    def set_username( self, new_username ):
+        """Method that modifies username of the user"""
+        self.username = new_username
+        return
+    def set_ip( self, new_ip ):
+        """Method that modifies IP of the user"""
+        self.ip_adr = new_ip
+        return
+    def set_all( self, new_username, new_ip ):
+        self.set_username( new_username )
+        self.set_ip( new_ip )
+        return
+    
+    def print_all( self ):
+        print("username: " + str(self.get_username())+"; ip: " + str(self.get_ip()) + ";" )
+        return
+    
+    
